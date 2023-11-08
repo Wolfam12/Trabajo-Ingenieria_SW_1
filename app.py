8f9a2aea42320ef435ef6dd23bc7e3ef33d27659
Wolfam Diaz Galeano <wolfamc.diazg@konradlorenz.edu.co>
2023-11-07 19:04:40 -0500

    Actualizacion
    jnm

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-users = {'wolfam': '123','raz': '1234'}  # Almacena los nombres de usuario y contraseñas en un diccionario (en la vida real, usarías una base de datos)
+users = {'wolfam': '123', 'raz': '1234'}  # Almacena los nombres de usuario y contraseñas en un diccionario (en la vida real, usarías una base de datos)
+
+# Conexión a la base de datos SQLite
+def conectar_bd():
+    conn = sqlite3.connect('Bdd\BasesDatosRegistraduria.db')
+    return conn
 
 @app.route('/')
 def home():
     if 'username' in session:
-        return render_template('home.html')
+        # Recupera los datos de la base de datos
+        connection = conectar_bd()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM Personas')
+        datos = cursor.fetchall()
+        connection.close()
+
+        return render_template('home.html', datos=datos)  # Pasa los datos a la plantilla
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -28,6 +41,22 @@ def login():
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
+@app.route('/mostrar_datos')
+def otra_pagina():
+    if 'username' in session:
+        # Conecta a la base de datos
+        connection = sqlite3.connect('BasesDatosRegistraduria.db')
+        cursor = connection.cursor()
+
+        # Ejecuta una consulta SQL para obtener los datos
+        cursor.execute('SELECT * FROM Personas')
+        datos = cursor.fetchall()
+
+        # Cierra la conexión a la base de datos
+        connection.close()
+
+        return render_template('mostrar_datos.html', datos=datos)
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
